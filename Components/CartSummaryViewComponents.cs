0c1c40bd3b93757af33ac4ef7a782fef7c73b171
iceberg1204 <36276633+iceberg1204@users.noreply.github.com>
2020-03-01 11:36:05 +0300

    Add files via upload

---
+++ Components/CartSummaryViewComponents.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Components
+{
+    public class CartSummaryViewComponents : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponents(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
