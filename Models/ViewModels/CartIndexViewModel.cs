fc81b054cd2fb694ab60d73fe5f3651c3a0a4d99
iceberg1204 <36276633+iceberg1204@users.noreply.github.com>
2020-03-01 12:26:18 +0300

    Add files via upload

---
+++ Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SportStore.Models;
+
+namespace SportStore.Models.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
