c5aa129b0d392a11c55eaad4429b39d4a5cb1caa
iceberg1204 <36276633+iceberg1204@users.noreply.github.com>
2020-03-01 12:14:32 +0300

    Add files via upload

---
+++ Infrastructure/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Newtonsoft.Json;
+
+namespace SportStore.Infrastructure
+{
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var SessionData = session.GetString(key);
+            return SessionData == null ? default(T) : JsonConvert.DeserializeObject<T>(SessionData);
+        }
+    }
+}
